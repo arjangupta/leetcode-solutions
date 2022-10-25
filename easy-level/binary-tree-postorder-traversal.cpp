5cf1b80280ceed087feb69c0f63cb06aba61c01a
Arjan Gupta <arjan.gupta@lindsay.com>
2022-10-25 14:57:35 -0500

    For post order traversal, need a way to know that a root has been visited already

--- easy-level/binary-tree-postorder-traversal.cpp
+++ easy-level/binary-tree-postorder-traversal.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 // Definition for a binary tree node.
 struct TreeNode {
@@ -13,10 +14,24 @@ struct TreeNode {
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
         vector<TreeNode*> stack;
         stack.push_back(root);
         while (!stack.empty()) {
-            
+            TreeNode* curr = stack[stack.size()-1];
+            stack.pop_back();
+            if (curr != nullptr) {
+                cout << curr->val << " ";
+                if (curr->left == nullptr && curr->right == nullptr) {
+                    result.push_back(curr->val);
+                }
+                else {
+                    stack.push_back(curr);
+                    stack.push_back(curr->right);
+                    stack.push_back(curr->left);
+                }
+            }
         }
+        return result;
     }
 };
