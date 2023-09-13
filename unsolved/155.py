19ee50231a048cf276f41da05cb788e875890026
Arjan Gupta <arjan.gupta@lindsay.com>
2023-09-13 16:32:22 -0500

    Add attempt on lc#155

---
+++ unsolved/155.py
@@ -0,0 +1,44 @@
+from typing import List
+
+class MinObject:
+    def __init__(self, arr):
+        self.min_indices = arr
+        self.shared_count = 0
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_ds: List[MinObject] = []
+
+    def push(self, val: int) -> None:
+        if len(self.stack) == 0 or val < self.getMin():
+            self.min_ds.append(MinObject([len(self.stack)]))
+        elif val == self.getMin():
+            self.min_ds[-1].min_indices.append(len(self.stack))
+        else:
+            self.min_ds[-1].shared_count += 1
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.min_ds[-1].shared_count > 0:
+            self.min_ds[-1].shared_count -= 1
+        else:
+            self.min_ds.pop()
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        idx = self.min_ds[-1].min_indices[0]
+        print("getMin idx:", idx)
+        return self.stack[idx]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
