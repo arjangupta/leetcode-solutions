9071ca46b54e88dd20dd29e6c44a1309d661fa24
Arjan Gupta <arjangupta95@gmail.com>
2024-02-14 10:13:57 -0600

    Solve easy-level binary search problem

---
+++ easy-level/704.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        half_index = int(len(nums)/2)
+        if nums[half_index] == target:
+            return half_index
+        if half_index == 0:
+            return -1
+        l_result = self.search(nums[0:half_index], target)
+        r_result = self.search(nums[half_index:], target)
+        if l_result > -1:
+            return l_result
+        elif r_result > -1:
+            return half_index + r_result
+        else:
+            return -1
